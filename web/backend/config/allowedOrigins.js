00b7fae1a1a52644ff21237dcf657b5738e6379e
Araish Shaikh <shaikh2003araish@gmail.com>
2023-10-07 10:23:24 +0530

    cors updated

--- web/backend/config/allowedOrigins.js
+++ web/backend/config/allowedOrigins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
     'http://localhost:3300',
     'http://localhost:3300/products',
     'http://localhost:3000',
