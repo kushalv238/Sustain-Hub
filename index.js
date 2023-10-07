40329375046c9e2af57b5c052e7de2ec8e739ad1
Nihar Nandoskar <niharnandoskar@gmail.com>
2023-10-07 09:24:44 +0530

    api created for seller regis and login

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+const authRoutes = require('./src/routes/auth');
+app.use('/seller', authRoutes);
+
+// Connect to MongoDB
+const MONGODB_URI = 'mongodb+srv://nihar:abcd12345@cluster0.4jfpspv.mongodb.net/temp?retryWrites=true&w=majority';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('Connected to MongoDB');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/controllers/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error registering user' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error logging in' });
+  }
+});
+
+module.exports = router;
